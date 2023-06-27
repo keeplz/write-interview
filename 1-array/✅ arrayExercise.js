ec17babaea2d0ffcd60bf6db9a11f794c0a2d301
彭乐政🍊 <penglz@lenovocloud.com>
2023-06-27 19:00:25 +0800

    chore: update arrayExercies
    use console.log and equal

--- 1-array/✅ arrayExercise.js
+++ 1-array/✅ arrayExercise.js
@@ -4,23 +4,23 @@ Array.prototype.filterMe = function (cb, thisArg) {};
 
 // Array.prototype.filterMe.call(null);
 // Array.prototype.filterMe.call([1, 2, 3], 123);
-// Array.prototype.filterMe.call([1, 2, 3], (v) => v < 3);
-// Array.prototype.filterMe.call("12345", (v) => v > 3);
+// console.log(Array.prototype.filterMe.call([1, 2, 3], (v) => v < 3));
+// console.log(Array.prototype.filterMe.call("12345", (v) => v > 3));
 // var arr = [1, , , , 4, 5, 6];
-// Array.prototype.filterMe.call(arr, (v) => v > 3);
+// console.log(Array.prototype.filterMe.call(arr, (v) => v > 3));
 
 Array.prototype.mapMe = function (cb, thisArg) {};
-// Array.prototype.mapMe.call(null);
-// Array.prototype.mapMe.call([1, 2, 3], 123);
-// Array.prototype.mapMe.call([1, 2, 3], (v) => v * 2);
-// Array.prototype.mapMe.call("12345", (v) => v * 2);
+// console.log(Array.prototype.mapMe.call(null));
+// console.log(Array.prototype.mapMe.call([1, 2, 3], 123));
+// console.log(Array.prototype.mapMe.call([1, 2, 3], (v) => v * 2));
+// console.log(Array.prototype.mapMe.call("12345", (v) => v * 2));
 // var arr = [1, , , , 4, 5, 6];
 // console.log(Array.prototype.mapMe.call(arr, (v) => v > 3));
 // console.log(Array.prototype.map.call(arr, (v) => v > 3));
 
 Array.prototype.forEachMe = function (cb, thisArg) {};
 // Array.prototype.forEachMe.call(null);
-// Array.prototype.forEachMe.call([1, 2, 3], 123);
+// Array.prototype.forEachMe.call([1, 2, 3], 123)
 // Array.prototype.forEachMe.call([1, 2, 3], (v) => {
 //   console.log(v);
 // });
@@ -30,6 +30,7 @@ Array.prototype.forEachMe = function (cb, thisArg) {};
 Array.prototype.reduceMe = function (cb, initialValue) {};
 // var a = [, , , , 1, 2, , , , , , 3];
 // console.log(Array.prototype.reduceMe.call(a, (a, b) => a + b));
+// console.log(Array.prototype.reduceMe.call(a, (a, b) => a + b, 1));
 // console.log(Array.prototype.reduceMe.call(null, (a, b) => a + b));
 // console.log(Array.prototype.reduceMe.call([, , ,], (a, b) => a + b));
 // console.log(Array.prototype.reduceMe.call([, , ,], (a, b) => a + b, 2));
